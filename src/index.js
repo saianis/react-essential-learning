ee152a70430cd89442c3a83edf4703515b9205eb
saianis <anish@Anishs-MacBook-Pro.local>
2020-04-01 01:55:33 +0530

    simple form example

--- src/index.js
+++ src/index.js
@@ -1,109 +1,135 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { exec } from 'child_process';
-
-let bookList = [
-  {"title": "Three mistakes of my life", "author": "Chetan Bhagat", "pages": 234},
-  {"title": "Two states", "author": "Chetan Bhagat", "pages": 354},
-  {"title": "One night at call center", "author": "Chetan Bhagat", "pages": 300}
-]
-
-// Child Component
-const Book = ({title, author, pages, freeBookmark}) => {
-  return (
-    <section>
-      <h2>{title}</h2>
-      <p>by: {author}</p>
-      <p>pages: {pages} pages</p>
-      <p>Free Bookmark Today: {freeBookmark ? 'yes!' : 'no!'}</p>
-    </section>
-  )
-}
-
-const Hiring = () => 
-    <div>
-      <p>
-        The library is hiring. Go to www.library.com/jobs for more.
-      </p>
-    </div>
-
-const NotHiring = () => 
-    <div>
-      <p>
-        The library is not hiring. Check back later for more info.
-      </p>
-    </div>    
 
 
+// let bookList = [
+//   {"title": "Three mistakes of my life", "author": "Chetan Bhagat", "pages": 234},
+//   {"title": "Two states", "author": "Chetan Bhagat", "pages": 354},
+//   {"title": "One night at call center", "author": "Chetan Bhagat", "pages": 300}
+// ]
 
-// Parent component
-// Note: Component lifecycle methods are only available when using class syntax and can't use them with function components
-class Library extends Component {
+// // Child Component
+// const Book = ({title, author, pages, freeBookmark}) => {
+//   return (
+//     <section>
+//       <h2>{title}</h2>
+//       <p>by: {author}</p>
+//       <p>pages: {pages} pages</p>
+//       <p>Free Bookmark Today: {freeBookmark ? 'yes!' : 'no!'}</p>
+//     </section>
+//   )
+// }
 
-  state = { 
-    open: true,
-    freeBookmark: true,
-    hiring: false,
-    data: [],
-    loading: false
-  }
+// const Hiring = () => 
+//     <div>
+//       <p>
+//         The library is hiring. Go to www.library.com/jobs for more.
+//       </p>
+//     </div>
 
-  componentDidMount() {
-    this.setState({loading : true})
-    fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
-          .then(data => data.json())
-          .then(data => this.setState({data, loading: false}))
-  }
+// const NotHiring = () => 
+//     <div>
+//       <p>
+//         The library is not hiring. Check back later for more info.
+//       </p>
+//     </div>    
 
-  toggleOpenClosed = () => {
-    this.setState( prevState => 
-      ({
-        open: !prevState.open
-      }
-    ))
-  }
+
+
+// // Parent component
+// // Note: Component lifecycle methods are only available when using class syntax and can't use them with function components
+// class Library extends Component {
+
+//   state = { 
+//     open: true,
+//     freeBookmark: true,
+//     hiring: false,
+//     data: [],
+//     loading: false
+//   }
+
+//   componentDidMount() {
+//     this.setState({loading : true})
+//     fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
+//           .then(data => data.json())
+//           .then(data => this.setState({data, loading: false}))
+//   }
+
+//   toggleOpenClosed = () => {
+//     this.setState( prevState => 
+//       ({
+//         open: !prevState.open
+//       }
+//     ))
+//   }
 
   
 
+//   render() {
+//     const { books } = this.props
+//     return (
+//       <div>
+//         {this.state.hiring ? <Hiring / > : <NotHiring />}
+//         {this.state.loading ? 'loading...' : 
+//           <div>
+//             {this.state.data.map (product => {
+//               return (
+//                 <div key={product.id}> 
+//                   <h3>Library product of the week!</h3>
+//                   <h4>{product.name}</h4>
+//                   <img alt={product.name} src={product.image} height={100}/>
+
+//                 </div>
+//               )
+//             })}
+
+//           </div>}
+//         <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
+//         <button onClick={this.toggleOpenClosed}>
+//           Change
+//         </button>
+//         {books.map(
+//           ( book, i) => 
+//             <Book 
+//               key= {i}
+//               title={book.title} 
+//               author={book.author} 
+//               pages={book.pages}
+//               freeBookmark={this.state.freeBookmark}/>
+//         )}
+//       </div>
+//     )
+
+//   }
+// }
+
+
+class FavoriteColorForm extends Component {
+  state = { value: ''}
+
+  newColor = e => this.setState({ value: e.target.value})
+
+  submit = e => {
+    console.log(`New Color: ${this.state.value}`)
+    e.preventDefault()
+    
+  }
   render() {
-    const { books } = this.props
     return (
-      <div>
-        {this.state.hiring ? <Hiring / > : <NotHiring />}
-        {this.state.loading ? 'loading...' : 
-          <div>
-            {this.state.data.map (product => {
-              return (
-                <div key={product.id}> 
-                  <h3>Library product of the week!</h3>
-                  <h4>{product.name}</h4>
-                  <img alt={product.name} src={product.image} height={100}/>
-
-                </div>
-              )
-            })}
-
-          </div>}
-        <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
-        <button onClick={this.toggleOpenClosed}>
-          Change
-        </button>
-        {books.map(
-          ( book, i) => 
-            <Book 
-              key= {i}
-              title={book.title} 
-              author={book.author} 
-              pages={book.pages}
-              freeBookmark={this.state.freeBookmark}/>
-        )}
-      </div>
+      <form onSubmit={this.submit}>
+        <label>Favorite Color:  
+          <input 
+            type="color"
+            onChange={this.newColor} />
+        </label>
+        <button>Submit</button>
+      </form>
     )
-
   }
 }
 
+
 render(
-  <Library books={bookList} />, 
+  <FavoriteColorForm />, 
   document.getElementById('root')
 )
