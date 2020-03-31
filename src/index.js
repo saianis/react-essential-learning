91bef8497b43da1cea138b0f0e678b53d65965f7
saianis <anish@Anishs-MacBook-Pro.local>
2020-03-31 23:59:39 +0530

    composing components

--- src/index.js
+++ src/index.js
@@ -1,47 +1,27 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-let skiData = {
-  total: 50,
-  powder: 20,
-  backCountry: 30,
-  goal: 100
-}
-
-const getPercent = decimal => {
-  return decimal * 100 + '%'
-}
-
-const calGoalProcess = (total, goal) => {
-  return getPercent(total/goal);
-}
-
-// Function components
-const SkiDayCounter = ({total, powder, backCountry, goal}) => {
+const Book = ({title, author, pages}) => {
   return (
     <section>
-      <div>
-        <p>Total Days: {total}</p>
-      </div>
-      <div>
-        <p>Powder Days: {powder}</p>
-      </div>
-      <div>
-        <p>Backcountry Days: {backCountry}</p>
-      </div>
-      <div>
-        <p>Goal Progress: {calGoalProcess(total, goal)}</p>
-      </div>
+      <h2>{title}</h2>
+      <p>by: {author}</p>
+      <p>pages: {pages} pages</p>
     </section>
   )
 }
 
+const Library = () => {
+  return (
+    <div>
+      <Book title='Three mistakes of my life' author='Chetan Bhagat' pages={300}/>
+      <Book title='Two states' author='Chetan Bhagat' pages={250}/>
+      <Book title='One night at Call Center' author='Chetan Bhagat' pages={350}/>
+    </div>
+  )
+}
+
 render(
-  <SkiDayCounter 
-  total={skiData.total}
-  powder={skiData.powder}
-  backCountry={skiData.backCountry}
-  goal={skiData.goal}
-  />, 
+  <Library />, 
   document.getElementById('root')
 )
