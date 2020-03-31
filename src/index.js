02ab1ef904fe8a8b485ad5b567f8dd4c9887b77c
saianis <anish@Anishs-MacBook-Pro.local>
2020-03-31 20:45:33 +0530

    added props to the component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// React Component
 
+// React Component
 //  Note: All component names must start with Uppercase letter
 class Message extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello All</h1>
+        <h1 style={{color: this.props.color}}>{this.props.msg}
+        </h1>
+        <p>I will come back in {this.props.minutes} minutes</p>
       </div>
     )
   }
@@ -17,6 +19,6 @@ class Message extends React.Component {
 
 
 ReactDOM.render(
-  <Message />, 
+  <Message color='blue' msg='how are you?' minutes={5}/>, 
   document.getElementById('root')
 )
