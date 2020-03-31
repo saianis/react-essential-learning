0d8e566073af960dc1e8a9c7cc285caabfd8621e
saianis <anish@Anishs-MacBook-Pro.local>
2020-03-31 21:09:07 +0530

    Added custom methods

--- src/index.js
+++ src/index.js
@@ -5,12 +5,22 @@ let skiData = {
   total: 50,
   powder: 20,
   backCountry: 30,
-  goal: 50
+  goal: 100
 }
 
 // React Component
 //  Note: All component names must start with Uppercase letter
 class SkiDayCounter extends Component {
+
+  // custom methods
+  getPercent = decimal => {
+    return decimal * 100 + '%'
+  }
+
+  calGoalProcess = (total, goal) => {
+    return this.getPercent(total/goal);
+  }
+
   render() {
     // ES6 destructuring 
     const {total, powder, backCountry, goal} = this.props
@@ -26,7 +36,7 @@ class SkiDayCounter extends Component {
           <p>Backcountry Days: {backCountry}</p>
         </div>
         <div>
-          <p>Goal: {goal}</p>
+          <p>Goal Progress: {this.calGoalProcess(total, goal)}</p>
         </div>
       </section>
     )
